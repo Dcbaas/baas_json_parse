6d77ba157b2bd38fa6d07b2a4be1eccb478abb64
Dcbaas <Dcbaas@users.noreply.github.com>
2022-06-17 11:41:59 -0400

    Add project files.

---
+++ baas_json_parse/baas_json_parse.cpp
@@ -0,0 +1,12 @@
+﻿// baas_json_parse.cpp : Defines the entry point for the application.
+//
+
+#include "baas_json_parse.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
